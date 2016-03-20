8f077cbc6582316a9694484059f78ac5cc584c89
Aleksi Gold <aleksi@kulta.us>
2016-03-20 20:54:49 +0200

    Add DB initialization

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
 require_relative 'controllers/application_controller'
+require_relative 'migrations/initial'
+
+if !File.exist?('db/links.db')
+  InitialMigration.migrate(:up)
+end
 
 ApplicationController.run!
